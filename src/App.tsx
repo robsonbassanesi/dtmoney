f114b8e44363202249f7c09dc9270823fd13c5f4
Robson Bassanesi <robson@cropper.com.br>
2022-06-23 15:25:27 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Header } from './components/Header/Header';
 import { Summary } from './components/Summary/Summary';
+import { TransactionsTable } from './components/TransactionsTable/TransactionsTable';
 import './global.scss';
 
 export function App() {
   return (
-    <div>
+    <>
       <Header />
       <Summary />
-    </div>
+      <TransactionsTable />
+    </>
   )
 }
 
--- src/components/TransactionsTable/TransactionsTable.tsx
+++ src/components/TransactionsTable/TransactionsTable.tsx
@@ -1,5 +1,33 @@
+import styles from './TransactionsTable.module.scss'
+
 export function TransactionsTable() {
   return (
-    <h1>TransactionsTable</h1>
+    <div className={styles.transactionsTable}>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr >
+            <td>Desenvolvimento de site</td>
+            <td className={styles.deposit}>R$12.000</td>
+            <td>dev</td>
+            <td>12/05/2022</td>
+          </tr>
+          <tr >
+            <td>Desenvolvimento de site</td>
+            <td className={styles.withdraw}>-R$12.000</td>
+            <td>dev</td>
+            <td>12/05/2022</td>
+          </tr>
+
+        </tbody>
+      </table>
+    </div>
   )
 }
